5b10625336aba607d7ad91f27fbb4f70b16b7767
lucasramos97 <lucasramosdev@gmail.com>
2021-09-06 14:23:19 -0300

    Set 8080 as default server port

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.management.commands.runserver import Command as runserver
 
 def main():
     """Run administrative tasks."""
@@ -17,6 +17,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
 if __name__ == '__main__':
+    runserver.default_port = '8080'
     main()
